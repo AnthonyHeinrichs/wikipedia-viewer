528d441ccf8cd0f04f46b8878eefb588cd719b0f
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-06-30 16:56:15 -0400

    Initial setup

---
+++ README copy.md
@@ -0,0 +1,7 @@
+# weather-app
+A simple web app that returns the weather/temperature for your location.
+#
+User Stories:
+* I can see the weather in my current location.
+* I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
+* I can push a button to toggle between Fahrenheit and Celsius.
--- src/App.js
+++ src/App.js
@@ -1,36 +1,5 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
-import Weather from './components/weather';
+
 export default function App() {
 
-  const [lat, setLat] = useState([]);
-  const [lon, setLon] = useState([]);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        setLat(position.coords.latitude);
-        setLon(position.coords.longitude);
-      });
-
-      await fetch(`https://weather-proxy.freecodecamp.rocks/api/current?lat=${lat}&lon=${lon}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
-      });
-    }
-    fetchData();
-  }, [lat,lon])
-
-  return (
-    <div className="app">
-      {(typeof data.main != 'undefined') ? (
-        <Weather weatherData={data}/>
-      ): (
-        <h1 className='main-header'>Please provide your location...</h1>
-      )}
-    </div>
-  );
 }
