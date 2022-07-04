d33f9cc4852743392e55e4544fa4bf322a7b4f48
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-07-04 15:25:58 -0400

    Setup button for random wiki article, need to connect to API request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 
 export default function App() {
+
+  const handleOnClick = () => {
+    console.log('Click!!')
+  }
+
   return (
-    <div>
-      <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-        Random Wikipedia Article
+    <div className='mt-20 grid place-items-center'>
+      <button onClick={handleOnClick} className="bg-gray-800 hover:bg-gray-600 text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+        Get a Random Wikipedia Article
       </button>
-      <h2>Search for an article</h2>
+      <p className='mt-6 text-white'>or...</p>
+      <h2 className='text-white'>Search for an Article</h2>
     </div>
   )
 }
