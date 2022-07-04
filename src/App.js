d7db78be9435df784d017e3afda5c81c0f7f7ac7
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-07-04 15:01:44 -0400

    Initial tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 
 export default function App() {
-  return <h1>Test</h1>
+  return (
+    <div>
+      <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+        Random Wikipedia Article
+      </button>
+      <h2>Search for an article</h2>
+    </div>
+  )
 }
